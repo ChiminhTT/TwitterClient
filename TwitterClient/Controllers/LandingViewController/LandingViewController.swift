2bb6bb936750cb27e663486fb59b639080ad68f1
ChiminhTT <0ho.maxence0@gmail.com>
2017-11-07 13:01:56 +0100

    Format

--- TwitterClient/Controllers/LandingViewController/LandingViewController.swift
+++ TwitterClient/Controllers/LandingViewController/LandingViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class LandingViewController: UIViewController {
+class LandingViewController: UIViewController
+{
 
-  override func viewDidLoad() {
+  // MARK: - Controller Life cycle
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning()
+  {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
