b75c07ff3992cfe85bdcc20a5593b5274721c601
ChiminhTT <0ho.maxence0@gmail.com>
2017-11-07 15:47:51 +0100

    Implement Search Query Builder

---
+++ TwitterClient/TwitterAPI/SearchQuery.swift
@@ -0,0 +1,39 @@
+//
+//  SearchQuery.swift
+//  TwitterClient
+//
+//  Created by maxence on 07/11/2017.
+//  Copyright © 2017 maxence_ho. All rights reserved.
+//
+
+typealias SearchQuery = String
+
+func buildSearcQuery(_ queryArgs: QueryArg...) -> SearchQuery
+{
+  let str = queryArgs.map({ $0.eval() })
+                     .reduce("", +)
+  return SearchQuery(str)
+}
+
+enum QueryArg
+{
+  case ScreenName(String)
+  case Count(Int)
+  case And
+}
+
+extension QueryArg
+{
+  func eval() -> String
+  {
+    switch self
+    {
+    case .ScreenName(let name):
+      return "screen_name=" + name
+    case .Count(let count):
+      return "count=" + String(count)
+    case .And:
+      return "&"
+    }
+  }
+}
