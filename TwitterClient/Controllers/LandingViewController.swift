f254043ce5520c6bcc973d1cd9650e3d4aa76c23
maxence <maxence@device-ac-1f-74-4f-f5-c9.home>
2017-11-07 12:13:25 +0100

    Add XCode project

---
+++ TwitterClient/Controllers/LandingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterClient
+//
+//  Created by maxence on 07/11/2017.
+//  Copyright © 2017 maxence_ho. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
